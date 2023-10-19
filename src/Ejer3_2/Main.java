030cc06cee901ce625b5a5eb695908f282b20c41
AndrenorAndiarson <shaadowsong@hotmail.com>
2023-10-19 11:52:52 -0400

    cambio1

--- src/Ejer3_2/Main.java
+++ src/Ejer3_2/Main.java
@@ -3,6 +3,8 @@ package Ejer3_2;
 public class Main {
     public static void main(String[] args) {
         Cadena cadena1 = new Cadena();
+        String mayus ="";
+        int n_mayus=0;
         int longitud = cadena1.cadenaInicial.length();
         System.out.println("La longitud del String es = "+longitud);
         String cadenaSinEspacios = cadena1.cadenaInicial.trim();
@@ -17,6 +19,14 @@ public class Main {
         System.out.println("El String remplazado= "+ cadenaRemplazada);
         boolean comparacion= cadenaRemplazada.equals("Programacion");
         System.out.println("Los String son iguales = "+ comparacion);
+
+        for (int i=0;i<cadena1.cadenaInicial.length();i++){
+            if(Character.isUpperCase(cadena1.cadenaInicial.charAt(i))){
+                n_mayus++;
+                mayus+=cadena1.cadenaInicial.charAt(i);
+            }
+        }
+        System.out.println(mayus);
 
     }
 }

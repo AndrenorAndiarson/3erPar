aba2bd5d639b1bfe7d9534ba5f8e624b928db147
AndrenorAndiarson <shaadowsong@hotmail.com>
2023-11-05 15:55:47 -0400

    holi holiiii

--- src/Ejer4_1/Cuenta.java
+++ src/Ejer4_1/Cuenta.java
@@ -2,33 +2,28 @@ package Ejer4_1;
 
 public class Cuenta {
     protected float saldo;
-    protected int numeroConsignaciones;
-    protected int numeroRetiros;
+    protected int numeroConsignaciones=0;
+    protected int numeroRetiros=0;
     protected float tasaAnual;
-    protected float comisionMensual;
+    protected float comisionMensual=0;
 
-    public Cuenta(float saldoInicial, float tasaAnual) {
-        this.saldo = saldoInicial;
+    public Cuenta(float saldo, float tasaAnual) {
+        this.saldo = saldo;
         this.tasaAnual = tasaAnual;
-        this.numeroConsignaciones = 0;
-        this.numeroRetiros = 0;
-        this.comisionMensual = 0.0f;
     }
 
     public void consignar(float cantidad) {
-        if (cantidad > 0) {
-            saldo += cantidad;
-            numeroConsignaciones++;
-        } else {
-            System.out.println("La cantidad a consignar debe ser mayor que cero.");
-        }
+
+       saldo =saldo+cantidad;
+       numeroConsignaciones=numeroConsignaciones+1;
     }
 
     public void retirar(float cantidad) {
-        if (cantidad > 0 && saldo >= cantidad) {
-            saldo -= cantidad;
-            numeroRetiros++;
-        } else {
+        float nuevoSaldo = saldo-cantidad;
+        if (nuevoSaldo>=0){
+            saldo-=cantidad;
+            numeroRetiros=numeroRetiros+1;
+                   }else {
             System.out.println("No es posible realizar el retiro. Verifique el saldo o la cantidad a retirar.");
         }
     }
